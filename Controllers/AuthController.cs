166336e161a6ae971cc9d81d84703cd4ceec6213
AnndyMadrigal <anndymadrigal@gmail.com>
2026-05-28 11:38:05 -0600

    Registro finalizado

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MultiservicosPiscinas.Models;
+using System.Security.Claims;
 
 namespace MultiservicosPiscinas.Controllers
 {
@@ -35,27 +38,50 @@ namespace MultiservicosPiscinas.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(Usuario usuario)
         {
-            usuario.RolId = 3; //rol de cliente por defecto
+            usuario.RolId = 3; //rol de cliente seteado por defecto en este caso
             usuario.FechaCreacion = DateTime.Now;
 
-            var result = await _context.Database.ExecuteSqlRawAsync(
-                "EXEC seg.InsertUser @p0, @p1, @p2, @p3, @p4, @p5, @p6",
-                usuario.RolId,
-                usuario.Nombre,
-                usuario.ApellidoPaterno,
-                usuario.ApellidoMaterno,
                 usuario.Correo,
                 usuario.Contrasena,
-                usuario.FechaCreacion
-            );
             if (result < 0)
             {
                 //mensaje en caso de error
                 ViewBag.Message = "Error al registrar el usuario.";
+                    usuario.ApellidoMaterno,
+                    usuario.FechaCreacion
+                )
+                .ToListAsync();
+
+                int nuevoUsuarioId = usuarioIdResult.FirstOrDefault();
+
+                if (nuevoUsuarioId <= 0)
+                {
+                    //mensaje en caso de error
+                    ViewBag.Message = "Error al registrar el usuario.";
+                    return View();
+                }
+                //luego de insertar en la tabla usuario, se inserta en cliente
+                string notasCliente = "Cliente registrado desde registro de usuario.";
+
+                var clienteResult = await _context.Database.ExecuteSqlRawAsync(
+                    "INSERT INTO cli.CLIENTE (usuario_id, notas) VALUES (@p0, @p1)",
+                    nuevoUsuarioId,
+                    (object)notasCliente ?? DBNull.Value
+                );
+
+                if (clienteResult <= 0)
+                {
+                    ViewBag.Mensaje= "Usuario creado, pero hubo un error al crear el perfil de cliente.";
+                    return View();
+                }
+
+                return RedirectToAction("Login", "Auth");
+            }
+                catch(Exception ex)
+            {
+                ViewBag.Mensaje = "Ocurrió un error inesperado en el servidor.";
                 return View();
             }
-
-            return RedirectToAction("Login", "Auth");
         }
 
         [HttpGet]
