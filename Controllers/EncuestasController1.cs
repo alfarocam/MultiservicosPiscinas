526fd9f5f8bae1a2b1d73765d0f383147a536d87
santiazo <azofeifasantiago1@gmail.com>
2026-05-31 20:27:51 -0600

    santi commit

---
+++ Controllers/EncuestasController1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiserviciosPiscinas.Models;
+
+namespace MultiserviciosPiscinas.Controllers
+{
+    public class EncuestasController : Controller
+    {
+        private readonly PiscinasYMultiserviciosContext _context;
+
+        public EncuestasController(PiscinasYMultiserviciosContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Crear(int servicioId)
+        {
+            var encuesta = new Encuestum
+            {
+                ServicioId = servicioId
+            };
+
+            return View(encuesta);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Encuestum encuesta)
+        {
+            bool existe = await _context.Encuesta
+                .AnyAsync(x => x.ServicioId == encuesta.ServicioId);
+
+            if (existe)
+            {
+                TempData["Error"] =
+                    "Este servicio ya posee una encuesta.";
+
+                return RedirectToAction("Index", "Portal");
+            }
+
+            encuesta.FechaEnvio = DateTime.Now;
+
+            _context.Encuesta.Add(encuesta);
+
+            await _context.SaveChangesAsync();
+
+            TempData["Exito"] =
+                "Encuesta registrada correctamente.";
+
+            return RedirectToAction("Index", "Portal");
+        }
+    }
+}
---
+++ Models/ContactoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiserviciosPiscinas.Models
+{
+    public class ContactoViewModel
+    {
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Correo { get; set; } = string.Empty;
+
+        public string Asunto { get; set; } = string.Empty;
+
+        public string Mensaje { get; set; } = string.Empty;
+    }
+}
