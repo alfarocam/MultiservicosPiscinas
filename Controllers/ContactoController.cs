94c4849e53157cb559eaf12482ccce7cc565830a
Daniel <Danibravo30@hotmail.com>
2026-06-01 16:49:24 -0600

    HU 3.1
    Enviar consulta por formulario de contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿/*
+using Microsoft.AspNetCore.Mvc;
 using MultiserviciosPiscinas.Models;
 
 namespace MultiserviciosPiscinas.Controllers
@@ -23,6 +24,44 @@ namespace MultiserviciosPiscinas.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Informacion()
+        {
+            return View();
+        }
+    }
+}
+*/
+using Microsoft.AspNetCore.Mvc;
+using MultiserviciosPiscinas.Models;
+
+namespace MultiserviciosPiscinas.Controllers
+{
+    public class ContactoController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Enviar(ContactoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            
+            TempData["Exito"] = "La consulta fue enviada correctamente. Nos pondremos en contacto contigo pronto.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        
         public IActionResult Informacion()
         {
             return View();
