212ec24cff65990a38deb2cc1293e9363100b077
Camila Alfaro <camilaalfa20@gmail.com>
2026-07-15 14:30:15 -0600

    Revert "CambiosTemp"
    This reverts commit c22c402154691885e0c421268abde3687d46b81b.

--- Program.cs
+++ Program.cs
@@ -21,7 +21,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 // Configurar DbContext con SQL Server
-builder.Services.AddDbContext<PiscinasYMultiserviciosContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); options.EnableDetailedErrors(); options.EnableSensitiveDataLogging(); });
+builder.Services.AddDbContext<PiscinasYMultiserviciosContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Pagina de errores para migraciones en desarrollo
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
